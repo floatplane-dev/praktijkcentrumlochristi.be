2f7b118a63877f600bc3904bc3b187db2408f3c7
Jan Werkhoven <jan.werkhoven@gmail.com>
2024-02-15 15:09:22 +1100

    Remove the book modal logic

--- src/js/app.js
+++ src/js/app.js
@@ -3,46 +3,5 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function init() {
-  prepareBookModal();
-}
-
-function prepareBookModal() {
-  document.querySelectorAll("button.book").forEach((button) => {
-    button.onclick = function () {
-      openModal();
-    };
-  });
-
-  document.querySelector("#modal .overlay").onclick = () => {
-    closeModal();
-  };
-
-  document.querySelector(`#modal #question-1 button.yes`).onclick = () => {
-    showModalContent("infectious");
-  };
-
-  document.querySelector(`#modal #question-1 button.no`).onclick = () => {
-    showModalContent("not-infectious");
-  };
-}
-
-function openModal() {
-  document.body.classList.add("show-modal");
-  document.querySelector("#page").style.top = `-${window.scrollY}px`;
-  showModalContent("question-1");
-}
-
-function closeModal() {
-  document.body.classList.remove("show-modal");
-  document.querySelector("#page").removeAttribute("style");
-  document.querySelectorAll("#modal section").forEach((element) => {
-    element.classList.add("hide");
-  });
-}
-
-function showModalContent(id) {
-  document.querySelector(`#modal section#${id}`).classList.remove("hide");
-  document.querySelectorAll(`#modal section:not(#${id})`).forEach((element) => {
-    element.classList.add("hide");
-  });
+  // No JS.
 }
